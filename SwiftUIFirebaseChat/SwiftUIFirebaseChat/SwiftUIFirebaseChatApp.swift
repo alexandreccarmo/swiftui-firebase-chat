021e111a349acecaf4f8ced00ebb505030bb3eb0
Alexandre Carmo <alexandre@alexandreccarmo.com>
2023-02-04 18:45:47 -0300

    estrutura inicial do projeto. Conexao com firabase e troca de dados em tempo real

---
+++ SwiftUIFirebaseChat/SwiftUIFirebaseChat/SwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatApp.swift
+//  SwiftUIFirebaseChat
+//
+//  Created by Alexandre C do Carmo on 04/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
